264e775e2f267316e52043e674f2615abe8b5b55
Smukello <101141116+Smukello@users.noreply.github.com>
2022-10-25 16:04:29 +0200

    Add files via upload

---
+++ Scans/scan_omp.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <omp.h>
+#include<iostream>
+
+using namespace std;
+
+// Fills prefix sum array using parallel implementation
+
+void scan(int out[], int in[])
+
+{
+    out[0] = in[0];
+
+    int n = 8;
+    #pragma omp parallel
+    {
+        #pragma omp parallel for scan(+: out)
+        // Adding present element with previous element
+        for (int i = 1; i < n; i++){
+            out[i] = out[i - 1] + in[i];
+        }
+    }
+}
+
+int main()
+
+{
+
+    int arr[] = {2, 1, 4, 0, 3, 7, 6, 3};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int prefixSum[n];
+
+    scan(prefixSum, arr);
+
+    for (int i = 0; i < n; i++){
+
+        cout << prefixSum[i] << " ";
+
+    }
+
+}
